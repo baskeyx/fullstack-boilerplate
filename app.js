56b4b2673678a831c294b7ad51414c35003cf96d
baskeyx <daniel.baskeyfield@gmail.com>
2022-07-28 11:02:28 +0100

    adding path

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const path = require('path');
+require('dotenv').config();
 const app = express();
 const port = 8000;
+const { ENVIRONMENT } = process.env;
 
 app.get('/api', (req, res) => {
   res.send({copy: 'Hello World!'});
@@ -9,3 +12,10 @@ app.get('/api', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+if (ENVIRONMENT !== 'DEV') {
+  app.use(express.static(path.join(__dirname, 'frontend/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+  })
+}
